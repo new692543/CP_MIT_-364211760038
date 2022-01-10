775bd771f0d15c3bf0b6914b3de2d1ede19f0188
USER <new692543>
2022-01-10 23:55:45 +0700

    Test upload to github

---
+++ Homework/assignment_1.py
@@ -0,0 +1,32 @@
+#Name: {Krerkkeat_Dummeee}
+#SID: {364211760038}
+#Group: {MIT212}
+
+x = 100  # ประกาศตัวแปร x มีค่าเท่ากับ 100
+print(x) # แสดงผลค่าตัวแปร x
+print(type(x)) # แสดงชนิดข้อมูลของตัวแปร x
+
+y =  200 # ประกาศตัวแปร  y มีค่าเท่ากับ 200
+print(y) # แสดงผลค่าตัวแปร y
+print(type(y)) # แสดงชนิดข้อมูลของตัวแปร y
+
+print('x is ',x,'and y is ',y,'') # แสดงผลค่าตัวแปร x และ y โดยมีข้อความดังนี้  "x is 100 and y is 200"
+
+z = x+y  # หาผลรวมของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+print(z) # แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+
+z = x-y  # หาผล][ของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+print(z) # แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+
+z = x*y  # หาผลคูณของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+print(z) # แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+
+z = x/z   # หาผลหารแบบจำนวนเต็ม (floor division) ของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+print(z)  # แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+
+z = x%z    # หาผลหารของตัวแปร x และ y และเก็บไว้ในตัวแปร z
+print(z)   # แสดงผลค่าตัวแปร z formatted print  -- > print(f{...})
+
+
+
+
